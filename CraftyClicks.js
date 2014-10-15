4c3cca66603747895b175869acf5e5d35e4c4a6a
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2014-10-15 13:07:15 +0100

    Initial Commit

---
+++ CraftyClicks.js
@@ -0,0 +1,118 @@
+/*!
+ * Crafty Clicks API Wrapper v1.0.0
+ * https://github.com/dwmsw/CraftyClicks.js
+ *
+ * Copyright 2014, Digital Web Media Limited
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ * Date: 2014-10-15
+ */
+
+
+/**
+ * Lets define the CraftyClicks Constructor
+ * @param {string} apiKey Your Crafty Clicks API Key
+ */
+function CraftyClicks(apiKey) {
+    
+    if (apiKey) {
+        this.apiKey = apiKey;
+    } else {
+        this.apiKey = 'xxxxx-xxxxx-xxxxx-xxxxx';
+    }
+    this.baseURL = '//pcls1.craftyclicks.co.uk/json/';
+    this.postCode = '';
+    this.result = '';
+
+}
+
+/**
+ * Returns the complete API Response
+ * @return {string} The JSON response
+ */
+CraftyClicks.prototype.getAll = function() {
+    return this.result
+};
+
+/**
+ * This is the main function to be used by the user
+ * @param  {string} searchType Whether to make a basic or rapid search
+ * @param  {string} postCode   The postcode to search upon
+ */
+CraftyClicks.prototype.search = function(searchType, postCode) {
+    // Check for a postcode and either set it or throw an exception
+    if (postCode) {
+        this.postCode = postCode;
+    } else {
+        throw 'No postcode supplied';
+    }
+
+    // Check the requested search type and either call the appropriate method, or throw exception
+    if (searchType == 'basic') {
+        this.result = this.BasicAddress();
+    } else if (searchType == 'rapid') {
+        this.result = this.RapidAddress();
+    } else {
+        throw 'Invalid Search Type';
+    }
+    
+    // Check for a response from Crafty Clicks that specifies an invalid API key used on a non-sandbox postcode
+    if (this.result.town == "Thank you, CraftyClicks.co.uk") {
+        throw "Please enter a valid API key or use the sandbox postcodes";
+    }
+  
+    // Check for proper errors and throw exceptions if they're found    
+    if (this.result.error_code) {
+        throw this.result.error_msg;
+    }
+};
+
+/**
+ * Make a RapidAddress Request
+ */
+CraftyClicks.prototype.RapidAddress = function() {
+    var endpoint = 'rapidaddress';
+    return this.makeRequest(endpoint);
+};
+
+/**
+ * Make a BasicAddress Request
+ */
+CraftyClicks.prototype.BasicAddress = function() {
+    var endpoint = 'basicaddress';
+    return this.makeRequest(endpoint);
+};
+
+/**
+ * Make a request to the CraftyClicks API
+ * @param  {string} endpoint The endpoint to fire requests on
+ * @return {string}          The JSON response
+ */
+CraftyClicks.prototype.makeRequest = function(endpoint) {
+
+    // Set up the URL
+    var url = this.baseURL + endpoint + '?key=' + this.apiKey + '&postcode=' + this.postCode;
+
+    // Create new XMLHttpRequest, has to be synchronous so we can handle response
+    request = new XMLHttpRequest();
+    request.open('GET', url, false);
+
+    // Wait for change and then either JSON parse response text or throw exception for HTTP error
+    request.onreadystatechange = function() {
+        if (this.readyState === 4){
+            if (this.status >= 200 && this.status < 400){
+                // Success!
+                data = JSON.parse(this.responseText);
+            } else {
+                throw 'HTTP Request Error';
+            }
+        }
+    };
+    // Send request
+    request.send();
+    // Nullify request object 
+    request = null;
+    // Return JSON data
+    return data;
+};
--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-CraftyClicks.js
-===============
+#CraftyClicks.js
 
-A wrapper for the CraftyClicks API
+CraftyClicks.js is a JavaScript wrapper for the [CraftyClicks](http://craftyclicks.co.uk) API.
+
+You can access the API Documentation at the following URL: [https://craftyclicks.co.uk/api/jsonp-api-user-guide/](https://craftyclicks.co.uk/api/jsonp-api-user-guide/)
+
+## Requirements
+This wrapper is completely standalone and has no dependency on jQuery or any other JavaScript library or framework.
+
+## Usage
+
+Using this wrapper is fairly simple, it's just a case of calling a new instance of `CraftyClicks` and passing it your API key, from there you can use the `search` method to make searches. 
+
+**Rapid Search**
+
+```javascript
+// Create a new instance of CraftyClicks
+var test = new CraftyClicks('YOUR API KEY HERE');
+// Pass it your preferred search method and postcode
+test.search('rapid', 'AA11AA');
+// Get the API response
+var output = test.getAll();
+// Log the output
+console.log(output);
+```
+
+**Basic Search**
+
+```javascript
+// Create a new instance of CraftyClicks
+var test = new CraftyClicks('YOUR API KEY HERE');
+// Pass it your preferred search method and postcode
+test.search('basic', 'AA11AA');
+// Get the API response
+var output = test.getAll();
+// Log the output
+console.log(output);
+```
+
+The wrapper will return the full API response, with no filtering. 
+
+I'm hoping to add some specific getters in a later version for items but for now this will have to be done manually!
